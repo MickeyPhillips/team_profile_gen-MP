3f35fb50828f25d8a83447a86b950375157cdd19
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-17 21:00:59 -0500

    added prompt

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,21 @@
 const Employee = require('../lib/Employee.js');
+
+test('creates new employee', () => {
+    const employee = new Employee('Michael Scott', 1, 'Michael@fakemail.com');
+
+    expect(employee.name).toBe('Michael Scott');
+    expect(employee.id).toEqual(1);
+    expect(employee.email).toBe('Michael@fakemail.com');
+})
+
+test('checks if employee has a role', () => {
+    const employee = new Employee('Michael Scott', 1, 'Michael@fakemail.com', 'Manager');
+
+    expect(employee.getRole()).toBeTruthy();
+
+    employee.role = 'Supreme Ruler of the Universe';
+
+    expect(employee.getRole()).toBeFalsy();
+
+})
+
