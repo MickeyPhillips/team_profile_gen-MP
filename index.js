57357eb9ef74b8b67c52a329d82b1bfde8cf6527
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-23 13:13:03 -0600

    updated Employee Object

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const Prompt = require('./lib/Prompt');
+const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
+const Employee = require('./lib/Employee');
 
-
-new Prompt()
-    .managerPrompt()
-    .then(nonManagerPrompt)
-    .then(employeeData => {
-        return generatePage(employeeData);
-    })
-    .then(pageHTML => {
-        return writeToFile(pageHTML);
-    })
-    .then(writeResponse => {
-        console.log(writeResponse);
-        return copyToFile();
-    })
-    .then(copyResponse => {
-        console.log(copyResponse);
-    })
-    .catch(err => {
-        console.log(err)
-    });
+// new Prompt()
+//     .managerPrompt()
+//     .then(nonManagerPrompt)
+//     .then(employeeData => {
+//         return generatePage(employeeData);
+//     })
+//     .then(pageHTML => {
+//         return writeToFile(pageHTML);
+//     })
+//     .then(writeResponse => {
+//         console.log(writeResponse);
+//         return copyToFile();
+//     })
+//     .then(copyResponse => {
+//         console.log(copyResponse);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
