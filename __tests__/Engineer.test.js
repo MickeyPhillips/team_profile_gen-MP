c343a03326029d355dc4cd74d0f3ca007dbbf1bd
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-23 13:35:51 -0600

    updated the __tests__ and the Intern and Manager objects

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,16 +1,20 @@
 const Engineer = require('../lib/Engineer.js');
 
 test('creates new Engineer', () => {
-    const employee = new Engineer('Michael Scott', 1, 'Michael@fakemail.com');
+    const employee = new Engineer('Michael Scott', 1, 'Michael@fakemail.com', 'Github.com/MichaelScottTheCodingGod');
 
-    expect(employee.name).toBe('Michael Scott');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('Michael@fakemail.com');
-    expect(employee.getRole()).toEqual('Engineer');
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.github).toEqual(expect.any(String));
 })
 
 test('checks if engineer', () => {
-    const employee = new Engineer('Michael Scott', 1, 'Michael@fakemail.com', 'Engineer', 'Github.com/Me');
+    const employee = new Engineer('Michael Scott', 1, 'Michael@fakemail.com', 'Engineer', 'Github.com/MichaelScottTheCodingGod');
 
-    expect(employee.github).toEqual(expect.any(String));
+    expect(employee.getName()).toBe(employee.name);
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getGithub()).toBe(employee.github);
+    expect(employee.getRole()).toBe('Engineer');
 })
