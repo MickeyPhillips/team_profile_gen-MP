f890105ba1c6789d51e2067644d656e8e2579617
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-23 13:16:26 -0600

    updated Engineer Object

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -13,8 +13,4 @@ test('checks if engineer', () => {
     const employee = new Engineer('Michael Scott', 1, 'Michael@fakemail.com', 'Engineer', 'Github.com/Me');
 
     expect(employee.github).toEqual(expect.any(String));
-    
-    employee.github = 80;
-
-    expect(employee.getGithub()).toBeFalsy();
 })
